31dec7b904eeb5ef8f599c604c4715751c7553e9
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-04-25 16:48:03 -0300

    feat: add navbar

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,19 @@
 import styles from "./homepage.module.css"
 
+import { Feature } from "@/components/feature/Feature"
+import { CategoryList } from "@/components/categoryList/CategoryList"
+import { CardList } from "@/components/cardList/CardList"
+import { Menu } from "@/components/menu/Menu"
+
 export default function Home() {
-  return <div></div>
+  return (
+    <div className={styles.container}>
+      <Feature />
+      <CategoryList />
+      <div className={styles.content}>
+        <CardList />
+        <Menu />
+      </div>
+    </div>
+  )
 }
--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,6 +1,11 @@
 import React from "react"
 import styles from "./cardList.module.css"
+import { Pagination } from "@/components/pagination/Pagination"
 
 export const CardList = () => {
-  return <div className={styles.container}>CardList</div>
+  return (
+    <div className={styles.container}>
+      <Pagination />
+    </div>
+  )
 }
